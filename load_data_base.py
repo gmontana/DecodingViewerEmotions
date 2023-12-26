68dfc667d61bf4cd12ac4736364f0fe0bcb64f93
Giovanni Montana <giovanni.montana@gmail.com>
2023-12-26 18:43:31 +0200

    refactor with documentation

--- load_data_base.py
+++ load_data_base.py
@@ -1,74 +1,105 @@
 import argparse
 import os
-from lib.utils.utils import loadarg
+import logging
+from lib.utils.utils import loadarg, load_pickle
+from mvlib.mvideo_lib import VideoDB
 
-import collections
-from collections import Counter
+# Setup logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-from mvlib.utils  import save_pickle, load_pickle , save_dict
-from mvlib.mvideo_lib  import Video , VideoDB
+
+def parse_arguments():
+    """
+    Parse command line arguments.
+
+    Returns:
+    -------
+    args : Namespace
+        The arguments namespace containing all command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Load and process a video database.")
+    parser.add_argument("--config", type=str,
+                        help="Path to the configuration file.", required=True)
+    args = parser.parse_args()
+    return args
+
+
+def load_configuration(config_path):
+    """
+    Load and validate the configuration file.
+
+    Parameters:
+    ----------
+    config_path : str
+        Path to the configuration file.
+
+    Returns:
+    -------
+    config : dict
+        The loaded and validated configuration dictionary.
+    """
+    if not os.path.exists(config_path):
+        logging.error(f"Configuration file does not exist: {config_path}")
+        raise FileNotFoundError(
+            f"Configuration file does not exist: {config_path}")
+
+    config = loadarg(config_path)
+
+    # Validate necessary keys in config
+    required_keys = ["dataset"]
+    for key in required_keys:
+        if key not in config:
+            logging.error(f"Missing required config key: {key}")
+            raise KeyError(f"Missing required config key: {key}")
+
+    return config
+
+
+def process_video_database(config):
+    """
+    Load and process the video database based on the provided configuration.
+
+    Parameters:
+    ----------
+    config : dict
+        The configuration dictionary containing necessary paths and parameters.
+
+    Returns:
+    -------
+    None
+    """
+    # Extracting file paths and directories from the configuration
+    dataset_config = config["dataset"]
+    video_db_file = dataset_config["fileVDB"]
+
+    if not os.path.exists(video_db_file):
+        logging.error(f"Video database file does not exist: {video_db_file}")
+        raise FileNotFoundError(
+            f"Video database file does not exist: {video_db_file}")
+
+    video_db = load_pickle(video_db_file)
+    # Example of a processing step, adjust as needed
+    video_db.filtrMarket(market_filtr=826)
+
+    # Additional processing steps can be added here
+
+    logging.info("Video database loaded and processed successfully.")
 
 
 def main():
+    """
+    Main function to orchestrate loading and processing a video database.
+    """
+    try:
+        args = parse_arguments()
+        config = load_configuration(args.config)
+        process_video_database(config)
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
+        raise
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str)
-    args_in = parser.parse_args()
-    args = loadarg(args_in.config)
-
-    fileDescriptionVideos = f'{args["dataset"]["fileDescriptionVideos"]}'
-    fileIndividualProfiles = f'{args["dataset"]["fileIndividualProfiles"]}'
-    fileVDB = f'{args["dataset"]["fileVDB"]}'
-    video_dir_path = f'{args["dataset"]["data_dir"]}/{args["dataset"]["dir_videos"]}'
-    frames_dir_path = f'{args["dataset"]["data_dir"]}/{args["dataset"]["dir_frames"]}'
-    audios_dir_path = f'{args["dataset"]["data_dir"]}/{args["dataset"]["dir_audios"]}'
-
-    VDB = load_pickle(fileVDB)
-    VDB.filtrMarket( market_filtr=826)
-    print("clip_length", VDB.clip_length, VDB.jump)
-
-    VDB.get_aggregated_profiles()  # self.APTV
-    VDB.add_Emotions()
-
-    clip_length = 5
-    VDB.get_dAPTV(clip_length)
-    jump = 0.5
-    VDB.get_dAPTV_porogs(jump, type="top")
-    VDB.get_positive_ID()
-    VDB.get_negative_ID()
-
-    #exit()
-    #dir_save = f'{args["dataset"]["data_dir"]}'
-    #VDB.split_train_validation_test(dir_save, rate=[80, 10, 10])
-    exit()
-
-    # videos --> frame&audio
-    if not os.path.exists(frames_dir_path):
-        os.makedirs(frames_dir_path)
-        os.makedirs(audios_dir_path)
-
-    VDB.parse_video_set(audios_dir_path, mode="audio")
-    VDB.parse_video_set(frames_dir_path,  mode ="frames")
-
-
-    exit()
-
-
-    # recompute VDB for new clip_length or jump
-    clip_length = 5
-    VDB.get_dAPTV(clip_length)
-    jump = 0.5
-    VDB.get_dAPTV_porogs(jump, type="top")
-
-    VDB.get_positive_ID()
-    VDB.get_negative_ID()
-
-
-
-
-
-             
-        
 
 if __name__ == '__main__':
     main()
