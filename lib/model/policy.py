f8a7f55767a629b42820f5388b83b44fbbcec9f7
Giovanni Montana <giovanni.montana@gmail.com>
2023-12-27 16:40:37 +0200

    added documentation to all functions

--- lib/model/policy.py
+++ lib/model/policy.py
@@ -1,5 +1,3 @@
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,6 +5,23 @@ import torchvision
 
 
 def freeze_layers(net, FilterPos, DataParallel=False):
+    """
+    Freeze specific layers of the network while allowing others to continue training.
+
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose layers are to be partially frozen.
+    FilterPos : list or dict
+        Identifiers (usually names or indices) of layers that should remain trainable.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    This function iterates through all parameters of the model and sets 'requires_grad' to False
+    to freeze layers. It then selectively re-enables training for layers specified in FilterPos.
+    """
     if DataParallel:
         for param_name, param in net.module.named_parameters():
             param.requires_grad = False
@@ -23,6 +38,24 @@ def freeze_layers(net, FilterPos, DataParallel=False):
 
 
 def unfreeze_layers(net, FilterNeg, DataParallel=False):
+    """
+    Unfreeze specific layers of the network while keeping others frozen.
+
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose layers are to be partially unfrozen.
+    FilterNeg : list or dict
+        Identifiers (usually names or indices) of layers that should remain frozen.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    This function iterates through all parameters of the model and sets 'requires_grad' to True
+    to unfreeze layers. It then selectively disables training for layers specified in FilterNeg.
+    """
+
     if DataParallel:
         for param_name, param in net.module.named_parameters():
             param.requires_grad = True
@@ -39,30 +72,73 @@ def unfreeze_layers(net, FilterNeg, DataParallel=False):
 
 
 def count_free_layers(net, DataParallel=False):
+    """
+    Count the number of trainable (free) and frozen layers in the network.
 
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose layers are to be counted.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the count of trainable layers and frozen layers, respectively.
+
+    Notes
+    -----
+    This function iterates through all parameters of the model and counts the number of parameters
+    that have 'requires_grad' set to True (trainable) and False (frozen).
+    """
+    count_free, count_freeze = 0, 0
     if DataParallel:
-        count_free, count_freeze = 0, 0
         for param in net.module.parameters():
             if param.requires_grad:
                 count_free += 1
             else:
                 count_freeze += 1
     else:
-        count_free, count_freeze = 0, 0
         for param in net.parameters():
             if param.requires_grad:
                 count_free += 1
             else:
                 count_freeze += 1
 
+        # Additional logging for models with few frozen parameters
         if count_freeze < 10:
             for name, param in net.named_parameters():
                 if not param.requires_grad:
                     print("freeze", name)
+
     return count_free, count_freeze
 
 
 def freeze_policy(args, net, epoch, optimizer, DataParallel=False):
+    """
+    Apply a freezing policy to the network based on the current epoch. This involves freezing
+    or unfreezing layers as specified in the 'args' configuration.
+
+    Parameters
+    ----------
+    args : dict
+        The script arguments or other configuration, specifically containing the 'net_optim_policy' dictionary
+        with 'freeze_layers' and 'unfreeze_layers' for the freeze/unfreeze schedule.
+    net : torch.nn.Module
+        The neural network model whose layers are to be frozen/unfrozen.
+    epoch : int
+        The current epoch number.
+    optimizer : torch.optim.Optimizer
+        The optimizer being used in the training.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    The function reads the freeze and unfreeze schedule from 'args' and applies the appropriate
+    freezing or unfreezing policy to the model's layers based on the current epoch.
+    """
     FilterFreezeA, FilterFreezeB = {}, {}
     for i in range(0, len(args.net_optim_policy["freeze_layers"]), 2):
         layer_id = args.net_optim_policy["freeze_layers"][i]
@@ -125,10 +201,33 @@ def gradient_policy(args, epoch, optimizer):
 
 
 def get_optim_policy(args, net, epoch, optimizer, DataParallel=False):
+    """
+    Apply the optimization policy for the network based on the current epoch. This involves adjusting
+    learning rates, freezing/unfreezing layers, and other optimization-related adjustments.
 
+    Parameters
+    ----------
+    args : dict
+        The script arguments or other configuration, specifically containing the 'net_optim_policy' dictionary
+        with various policies for optimization.
+    net : torch.nn.Module
+        The neural network model to apply optimization policies to.
+    epoch : int
+        The current epoch number.
+    optimizer : torch.optim.Optimizer
+        The optimizer being used in the training.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    This function orchestrates the application of various optimization policies including freezing layers,
+    adjusting learning rates, and other epoch-dependent modifications.
+    """
     freeze_policy(args, net, epoch, optimizer, DataParallel)
     gradient_policy(args, epoch, optimizer)
 
+    # Additional policies based on the epoch
     if args.net_optim_policy["freeze_BN_layers"] < epoch:
         freeze_BN_layers(net, DataParallel)
 
@@ -138,30 +237,55 @@ def get_optim_policy(args, net, epoch, optimizer, DataParallel=False):
     count_free, count_freeze = count_free_layers(
         net, DataParallel=DataParallel)
     print("count_free_layers, count_freeze_layers", count_free, count_freeze)
-
     print("args.net_optim_policy", args.net_optim_policy)
     print("run_epoch lr", optimizer.param_groups[-1]['lr'], "epoch:", epoch)
 
 
-def freeze_BN_layers(net,  DataParallel=False):
+def freeze_BN_layers(net, DataParallel=False):
+    """
+    Freeze Batch Normalization (BN) layers in the network.
 
-    print("freeze_BN_layers")
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose BN layers are to be frozen.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
 
+    Notes
+    -----
+    This function iterates through all parameters of the model and sets 'requires_grad' to False
+    for parameters in Batch Normalization layers to freeze them.
+    """
     if DataParallel:
         for param_name, param in net.module.named_parameters():
-            if "norm" in param_name:
-                param.requires_grad = False
-            if "bn" in param_name:
+            if "norm" in param_name or "bn" in param_name:
                 param.requires_grad = False
     else:
         for param_name, param in net.named_parameters():
-            if "norm" in param_name:
-                param.requires_grad = False
-            if "bn" in param_name:
+            if "norm" in param_name or "bn" in param_name:
                 param.requires_grad = False
 
 
 def freeze_BLOCK_layers_hard(net, epoch,  DataParallel=False):
+    """
+    Apply a hard freeze policy to specific blocks of layers in the network based on the current epoch.
+
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose block layers are to be frozen/unfrozen.
+    epoch : int
+        The current epoch number.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    This function applies a hard freeze policy, meaning it completely freezes or unfreezes
+    entire blocks of layers based on the current epoch. The specific blocks and epochs are
+    determined by the training schedule.
+    """
 
     print("freeze_BLOCK_layers_hard", epoch)
 
@@ -184,6 +308,24 @@ def freeze_BLOCK_layers_hard(net, epoch,  DataParallel=False):
 
 
 def freeze_BLOCK_layers_soft(net, epoch,  DataParallel=False):
+    """
+    Apply a soft freeze policy to specific blocks of layers in the network based on the current epoch.
+
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose block layers are to be softly frozen/unfrozen.
+    epoch : int
+        The current epoch number.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    This function applies a soft freeze policy, meaning it selectively freezes or unfreezes
+    layers within specific blocks based on the current epoch. The specific blocks and epochs are
+    determined by the training schedule.
+    """
 
     print("freeze_BLOCK_layers_soft", epoch)
 
@@ -204,6 +346,22 @@ def freeze_BLOCK_layers_soft(net, epoch,  DataParallel=False):
 
 
 def restart_fc_layers(net, DataParallel=False):
+    """
+    Reinitialize the weights of fully connected (fc) layers in the network.
+
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The neural network model whose fc layers are to be reinitialized.
+    DataParallel : bool, optional
+        Flag indicating whether the model is wrapped with DataParallel. Default is False.
+
+    Notes
+    -----
+    This function reinitializes the weights of the fully connected layers to their default
+    initialization state. This might be used in transfer learning or other scenarios where
+    reinitialization of certain layers is beneficial.
+    """
 
     print("restart_fc_layers")
 
