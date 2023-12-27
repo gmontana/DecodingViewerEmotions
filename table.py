6dd15a09b477608e3199af460282467a3daaa37f
Giovanni Montana <giovanni.montana@gmail.com>
2023-12-27 18:16:34 +0200

    added docs

--- table.py
+++ table.py
@@ -4,8 +4,8 @@ import time
 
 import json
 import argparse
-from lib.utils.utils import loadarg,  AverageMeter , get_num_class
-from lib.utils.set_folders import check_rootfolders , get_file_results , define_rootfolders
+from lib.utils.utils import loadarg,  AverageMeter, get_num_class
+from lib.utils.set_folders import check_rootfolders, get_file_results, define_rootfolders
 
 
 def mknewfolder(folder):
@@ -16,7 +16,6 @@ def mknewfolder(folder):
         os.mkdir(folder)
 
 
-
 def main():
 
     global args
@@ -24,9 +23,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda_ids", type=str)  # 0_1_2_3
     args_in = parser.parse_args()
-
-
-
 
     script = "train.py"
     agrs_json = "config/adcumen1_ortigia_BG.json"
@@ -51,12 +47,15 @@ def main():
          "logs/screen_multiclass_may/ad_inet_16f_8f_4f_j05_BG4000/1_1_2_3_4_5_6_7_8_0.5_0.8_0.01_1_resnet50_16_0_1_adcumen"],
 
         ["RGB", "4", "INET21K",  "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_4_0_0_adcumen"],
-        ["RGB_audio", "4", "INET21K",  "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_4_0_1_adcumen"],
+        ["RGB_audio", "4", "INET21K",
+            "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_4_0_1_adcumen"],
         ["RGB", "8", "INET21K",     "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_8_0_0_adcumen"],
-        ["RGB_audio", "8", "INET21K", "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_8_0_1_adcumen"],
+        ["RGB_audio", "8", "INET21K",
+            "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_8_0_1_adcumen"],
 
         ["RGB", "16", "INET21K",     "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_16_0_0_adcumen"],
-        ["RGB_audio","16", "INET21K","logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_16_0_1_adcumen"],
+        ["RGB_audio", "16", "INET21K",
+            "logs/screen_multiclass_may/ad_timm_16f_8f_4f_j05_BG4000/0_1_2_3_4_5_6_7_8_0.5_0.5_0.1_1_resnet50_timm_16_0_1_adcumen"],
 
     ]
 
@@ -97,15 +96,14 @@ def main():
     results_screen = {}
     file_results_screen = f'tmp/results/{screen_id}.all'
 
-    for i,[modality, frames, pretrained, dir] in enumerate(dirs_p1):
+    for i, [modality, frames, pretrained, dir] in enumerate(dirs_p1):
         results_screen[i] = {}
 
         run_id = f"{screen_id}_{modality}_{frames}_{pretrained}"
         cmd = f"python3 {script} --config {agrs_json} --run_id {run_id} --cuda_ids {args_in.cuda_ids} --model {dir}"
-        print("cmd\n",cmd)
+        print("cmd\n", cmd)
 
         os.system(cmd)
-
 
         mode_train_val = ["validation", "test"]
         for mode in mode_train_val:
@@ -113,13 +111,14 @@ def main():
             print("open:", file_results)
             with open(file_results) as f:
                 results = json.load(f)
-                results_screen[i][mode] = [modality, frames, pretrained, results]
+                results_screen[i][mode] = [
+                    modality, frames, pretrained, results]
 
-        #break
-
+        # break
 
     with open(file_results_screen, 'w') as f:
         json.dump(results_screen, f, indent=4)
+
 
 if __name__ == '__main__':
     main()
